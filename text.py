3c79512de6aeaa8b25fa91ceeac87c26a736e299
sandy chen <sahdy5632@gmail.com>
2018-08-21 23:21:33 +0800

    成績計算

---
+++ text.py
@@ -0,0 +1,12 @@
+scroce=int(input("輸入成績"))
+if 90<= scroce <=100:
+    grade="A"
+elif 80 <= socroce <90:
+    grade="B"
+elif 70 <= socroce <80:
+     grade="C"
+elif 60<= socroce <70:
+    grade="D"
+else:
+    grade="F"
+print("成績是",grade)
